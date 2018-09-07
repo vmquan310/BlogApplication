78cdac0c8713b0701a3d9ebc5522bf0cd2717e07
Quan Vu Minh <quanvum1@HarveyNash.vn.local>
2018-09-07 17:46:58 +0700

    First commit

---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Models
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Picture { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
